36b4984dd67f30c5f496a14bd1e148c8aad76e0c
Sarwadnya Mutkule <sarwadnyamutkule99@gmail.com>
2026-03-24 22:55:59 +0530

    creating the structure to add Mongo to our repo

--- ParkingLot/ParkingLot/Program.cs
+++ ParkingLot/ParkingLot/Program.cs
@@ -1,10 +1,15 @@
+using Parking.Models;
+using Parking.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<MongoSetting>(builder.Configuration.GetSection("MongoSetting"));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<MongoRepository, MongoRepository>();
 
 var app = builder.Build();
 
